2b0d61190499949a78094cba3a6922f73b1895a2
Widiarta <widiartaman@gmail.com>
2020-10-15 20:26:57 +0800

    load file html

---
+++ WpfApp1/View/UserControl1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.View
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+            this.htmlSample.Navigate(new Uri(String.Format("file:///{0}/Assets/Html/01.html", curDir)));
+        }
+    }
+}
